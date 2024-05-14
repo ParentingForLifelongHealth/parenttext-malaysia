22d7e2808290296cb1fe6f5fddcf6bc3edbe0765
fagiothree <32491856+fagiothree@users.noreply.github.com>
2024-05-14 11:54:31 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,19 +4,39 @@
 localised_sheets = "13XUkS7VbGJSg1xiOb2tVOIFcoiochpxqaQeBTk2Ibns"
 
 # shared with all deployments
-T_C_onboarding_ID = "12ddvTz_ZfC-9-b0yxjVrSzczciUUE3GosLUFeOLIv9I"
+
+# messages in templates
+#T_C_onboarding_ID = "12ddvTz_ZfC-9-b0yxjVrSzczciUUE3GosLUFeOLIv9I"
+#T_content_ID = "1hcH8pFdiHZN0UvZgyv3Zht9ARBTx-VXhNBI2o8L7fHU"
+#safeguarding = "1PHgUhJnZdE0lK6C9teK-hwA6Tf-6Pgj1_OVdxoTgVOA"
+#T_delivery_ID = "1yf6T8FsNF5SIS7ktj05Wj7ha_Hkfrf66r63kfUWhJbI"
+#T_C_menu_ID = "1lf80mIiuv_F6xAa9j5zGvXas50WxdSsLj6vrPccGNwY"
+
+
+#C_modules_ID = "1435mczruh5CZoI7u0fWNuKGnJw062Si9j7er_NOwqT0"
+#C_dictionaries_ID = "1uc4WOOlyHTEV8fUGb8nPCYcPj446TRtsV8fucrOCxC4"  # update?
+#C_home_activity_checkin_ID = "112bPyJgsE2BHxeJ7AN61o_5Uvf94YUCBR_drdAU5tHM"
+#C_ltp_activities_ID = "1AdcFsatjGdbDYRqgw359JEhSZn6nkRbZHVZr-5yQe6I"
+#C_goal_checkin_ID = "115XSdB5AJ9A9Nwr7Cs1PER5htOjctWcVTnqxipFiRUY"
+#C_dev_asess_tool_ID = "1azmH-v6DIJe6w_hpkvkI5scla2pgcC5c3Iyg7hBkyq8"
+#>
+
 T_content_ID = "1hcH8pFdiHZN0UvZgyv3Zht9ARBTx-VXhNBI2o8L7fHU"
-safeguarding = "1PHgUhJnZdE0lK6C9teK-hwA6Tf-6Pgj1_OVdxoTgVOA"
-T_delivery_ID = "1yf6T8FsNF5SIS7ktj05Wj7ha_Hkfrf66r63kfUWhJbI"
-T_C_menu_ID = "1lf80mIiuv_F6xAa9j5zGvXas50WxdSsLj6vrPccGNwY"
-
-
+onboarding_data = "1_m_Plv3EgM6bSyokcI4qgY5sCE8s6zf4Bz1qli2zDSk"
+edited_onboarding = "1Sl0Jl_N4cGQi2INmE_EnX_aYUMUrUB6cKbuWVPzirtY"
 C_modules_ID = "1435mczruh5CZoI7u0fWNuKGnJw062Si9j7er_NOwqT0"
 C_dictionaries_ID = "1uc4WOOlyHTEV8fUGb8nPCYcPj446TRtsV8fucrOCxC4"  # update?
-C_home_activity_checkin_ID = "112bPyJgsE2BHxeJ7AN61o_5Uvf94YUCBR_drdAU5tHM"
 C_ltp_activities_ID = "1AdcFsatjGdbDYRqgw359JEhSZn6nkRbZHVZr-5yQe6I"
 C_goal_checkin_ID = "115XSdB5AJ9A9Nwr7Cs1PER5htOjctWcVTnqxipFiRUY"
 C_dev_asess_tool_ID = "1azmH-v6DIJe6w_hpkvkI5scla2pgcC5c3Iyg7hBkyq8"
+safeguarding_data = "1BKwGQJvtvIlZhirvaZ5UFOmMxVe0pB4eM3_4F0_Snu4"
+edited_safeguarding = "1bWOyM5yShTTJSaxwqRCrjUzkwbp7DF6_nSF_96YcZ2c"
+edited_delivery = "1q6E2c4Bg_UvqTmhxAsTIQngwAtj0aFoqu8wsPHnqmaU"
+delivery_data = "1JcWCrDWTWQsg5yV0wHmeS-Ln8JPpVET5U6akOJIFUK4"
+edited_menu = "1lIiFjZKS0eXzzo6XwDdqYv4e1A73WFCpWZg5ju-tCZE"
+menu_data = "1W42uFzCBUbiYXCUh-nxgCDMsjcJMC4IZXG00TVGUZxA"
+
+
 
 
 # "filename" is how it will be generally named in the pipeline.
@@ -25,30 +45,34 @@ C_dev_asess_tool_ID = "1azmH-v6DIJe6w_hpkvkI5scla2pgcC5c3Iyg7hBkyq8"
 # tags are used to identify flows to be process
 # "split_no" is used to divide the file at the final step to get it to a
 # manageable size that can be uploaded to rapidpro
+
 sources = [
     {
         "filename": "parenttext_del",
         "spreadsheet_ids": [
-            T_C_onboarding_ID,
+            onboarding_data,
+            edited_onboarding,
             C_ltp_activities_ID,
-            T_delivery_ID,
+            edited_delivery,
+            delivery_data,
             C_modules_ID,
             C_dictionaries_ID,
-            C_home_activity_checkin_ID,
-            T_C_menu_ID,
+            edited_menu,
+            menu_data,
             C_goal_checkin_ID,
             T_content_ID,
             C_dev_asess_tool_ID,
-            safeguarding,
+            safeguarding_data,
+            edited_safeguarding,
             localised_sheets],
         # crowdin names: home_activity_checkin_child dev_assess_tools
         # modules_child delivery_menu onboarding goal_checkins_child
-        "crowdin_name": "onboarding",
+        "crowdin_name": "delivery_menu",
         # possible values for tag 1: onboarding dev_assess ltp_activity
         # home_activity_checkin module goal_checkin safeguarding menu delivery
-        "tags": [2, "malaysia", 3, "child"],
+        "tags": [1,"onboarding",2, "malaysia", 3, "child"],
         "split_no": 1,
-        "archive": "https://drive.usercontent.google.com/download?id=1l_43DHz-WUZkrfouJ-GM4QxEA-2exlRy&export=download&authuser=0&confirm=t&uuid=6341bfe2-5b61-4366-a25f-86a04dc80a80&at=APZUnTXFyA2XHo5xC9fmls7UG3PA:1699289412671",  # noqa: E501
+        #"archive": "https://drive.usercontent.google.com/download?id=1l_43DHz-WUZkrfouJ-GM4QxEA-2exlRy&export=download&authuser=0&confirm=t&uuid=6341bfe2-5b61-4366-a25f-86a04dc80a80&at=APZUnTXFyA2XHo5xC9fmls7UG3PA:1699289412671",  # noqa: E501
      },
 ]
 
@@ -77,6 +101,7 @@ outputpath = "./output"
 # 3 - We may not want to do anything, for this use reference code "none"
 qr_treatment = "reformat"
 
+
 # This is the default phrase we want to add in if the quick replies are being moved to message text
 select_phrases = "./edits/select_phrases.json"
 
@@ -100,7 +125,7 @@ ab_testing_sheet_ID = "1i_oqiJYkeoMsYdeFOcKlvvjnNCEdQnZlsm17fgNvK0s" #same for a
 localisation_sheet_ID = "1RhsMtslpwyTmMLsZJZaaNqweKX7ONpmwR4-rQVpo_qw" #malaysia specific
 
 #Google sheet ID containing dict edits data
-eng_edits_sheet_ID = "13KkCGpENgOUs8smPa7clXwsqurzroMkB0BUMiXF4Tvc" #same for all deployments
+eng_edits_sheet_ID = "1Ab8H_s26EuOiS4nZ6HGADjD4CZw55586LL66fl8tEWI" #same for all deployments
 transl_edits_sheet_ID = "1tbwzqlie87UMOe7yhdeeUxkTa4NtqRyivgX9NtxAFe8" #south africa specific
 
 #Data used in safeguarding script
