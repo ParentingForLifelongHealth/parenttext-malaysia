1f2c7d9000fa2367933922936d37dcb1c7dcc197
fagiothree <32491856+fagiothree@users.noreply.github.com>
2024-05-24 16:40:03 +0200

    Update parenttext_models.py

--- models/parenttext_models.py
+++ models/parenttext_models.py
@@ -4,23 +4,5 @@ from parenttext_pipeline.models.parenttext_models import *
 from typing import List
 
 
-#####
-# content delivery data models (Malaysia specific)
-class Language(ParserModel):
-    eng: str = ""
-    msa: str = ""
 
 
-
-class ModuleDataModel(DataRowModel):
-    topic_ID: str = ""
-    priority_in_topic: str = ""
-    age: List[int] = []
-    child_gender: List[str] = []
-    name: Language = Language()
-
-class GoalTopicLinkModel(DataRowModel):
-    goal_id_c: str = ""
-    priority_in_goal_c: str = ""
-    goal_id_p: str = ""
-    priority_in_goal_p: str = ""
