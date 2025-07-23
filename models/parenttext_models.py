fee419a911bd3b4153ed3dbf60a54acb668ad1c2
Chiara Facciolà <chiara.facciola@idems.international>
2025-07-23 11:31:15 +0200

    update models to overwrite comic and tip blocks

--- models/parenttext_models.py
+++ models/parenttext_models.py
@@ -1,13 +1,14 @@
 from rpft.parsers.creation.datarowmodel import DataRowModel
 from rpft.parsers.common.rowparser import ParserModel
+
+
+
 from parenttext_pipeline.models.parenttext_models import *
 from typing import List
 
 
 
 
-del ComicBlockModel
-del TipsBlockModel
 
 class ComicBlockModel(ParserModel):
     intro: list[str] = []
@@ -20,3 +21,21 @@ class TipsBlockModel(ParserModel):
     intro: list[str] = []
     next_button: str = ""
     message: list[TipModel] = []
+
+class PlhContentModel(DataRowModel):
+    module_name: str = ""
+    pause_id: str = ""
+    survey_start_id: str = ""
+    survey_end_id: str = ""    
+    introduction: IntroductionBlockModel = IntroductionBlockModel()
+    importance: ImportanceBlockModel = ImportanceBlockModel()
+    quiz: QuizBlockModel = QuizBlockModel()
+    tips: TipsBlockModel = TipsBlockModel()
+    comic: ComicBlockModel = ComicBlockModel()
+    home_activity: HomeActivityBlockModel = HomeActivityBlockModel()
+    video: VideoBlockModel = VideoBlockModel()
+    audio: AudioBlockModel = AudioBlockModel()
+    congratulations: CongratulationsBlockModel = CongratulationsBlockModel()
+    opt_video: OptVideoBlockModel = OptVideoBlockModel()
+    audio_tip: AudioTipBlockModel = AudioTipBlockModel()
+    attached_single_doc: str = ""
